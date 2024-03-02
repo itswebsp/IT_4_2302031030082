1865be782d1a90f6cfe55f8a9ee2ca7bc8eb5dd5
patel sapna om <161123517+itswebsp@users.noreply.github.com>
2024-03-02 16:17:58 +0530

    Add files via upload

---
+++ Assignment-2/login.css
@@ -0,0 +1,18 @@
+responsive-transparent-login-f-icon {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 5301px;
+    height: 3913px;
+    object-fit: cover;
+  }
+  .login-page {
+    width: 100%;
+    height: 3913px;
+    position: relative;
+    background-color: #fff;
+    overflow: hidden;
+  }
+  
+  
+  
